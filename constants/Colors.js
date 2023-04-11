2ead33f65c6ca74f8595a901646a8ac44d198366
Mariana Soltis <marianabs_15@yahoo.com.ar>
2023-04-11 19:55:33 -0300

    first commit

---
+++ constants/Colors.js
@@ -0,0 +1,15 @@
+export default {
+  DARK_SIENNA: "#34c4cc",
+  celeste: '#34c4cc',
+
+  MAROON: "#34c4cc",
+
+  BLUSH: "#00c201",
+  verde: '#00c201',
+
+  LIGTH_PINK: "#f9bdd7",
+  rosa: '#f9bdd7',
+
+  PEACH_PUFF: "#f8a718",
+  naranja: '#f8a718',
+};
